49117aa2ef86f2865befd8e226599a6b66dc07b7
Mariusz Rosa <m.rosa@ebros.pl>
2017-05-22 23:07:38 +0200

    start commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}, ${req.method}, ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('log.txt', log + '\n', (err) => {
+        if (err) {
+            console.log(err);
+        }
+    })
+    next();
+});
+
+app.use((req, res, next) => {
+   res.render('update.hbs', {
+       title: 'site update', 
+       welcomeMessage: 'naprawa czekajcie', 
+       h1: 'remont'
+   })
+});
+
+hbs.registerHelper('getYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIT', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        title: 'home page titlelelele',
+        welcomeMessage: "widajcie na stronie",
+        h1: 'h1 nazwa'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        title: 'about titlelelele',
+        h1: 'about'
+    });
+})
+
+app.listen(3000, () => {
+    console.log('server start');
+});
